e1d7e3006aa145029c389b5155b5abaf764554a3
Santiago Molano <santiago.molano@solucionessyh.com>
2025-06-09 14:56:28 -0500

    fix(SETUP-02): elimina llamado a método de creación de tablas Se realiza el ajuste debido a que se va a realizar esta operación a través de migraciones para evitar errores en creación de tablas Refs #21

--- app/core/database/operations.py
+++ app/core/database/operations.py
@@ -11,10 +11,30 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+# Operaciones de base de datos para crear, eliminar tablas y verificar conexión
+# Utilizar unicamente en entorno de desarrollo o pruebas
+
 def create_tables():
     try:
         Base.metadata.create_all(bind=engine)
         logger.info("Tablas de base de datos creadas exitosamente.")
     except Exception as e:
         logger.error(f"Error en creacion de tablas: {e}")
+        raise
+
+def drop_all_tables():
+    try:
+        Base.metadata.drop_all(bind=engine)
+        logger.info("Todas las tablas de la base de datos han sido eliminadas.")
+    except Exception as e:
+        logger.error(f"Error al eliminar tablas: {e}")
+        raise
+
+def check_database_connection():
+    try:
+        with Session(engine) as session:
+            session.execute("SELECT 1")
+        logger.info("Conexión a la base de datos exitosa.")
+    except Exception as e:
+        logger.error(f"Error al conectar a la base de datos: {e}")
         raise
