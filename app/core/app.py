8111805f4818f95556cee8ee3d6abf8c21ee772c
Santiago Molano <santiago.molano@solucionessyh.com>
2025-06-09 11:52:16 -0500

    fix(SETUP-01): import de base de datos comentado Todavía no se implememnta la conexión ni el inicio de base de datos Refs #20

--- app/core/app.py
+++ app/core/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config.settings import settings
-from app.core.database import create_tables
+# from app.core.database import create_tables
 
 def create_app() -> FastAPI:
 
