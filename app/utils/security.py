0654c66ef51a1f32543ade5a5e7ec0b07173a6f0
Santiago Molano <santiago.molano@solucionessyh.com>
2025-06-17 08:45:17 -0500

    fix(feature/login): ajuste en formato de expiración de token de refresco Refs #31

--- app/utils/security.py
+++ app/utils/security.py
@@ -39,7 +39,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
 def create_refresh_token(data: dict) -> str:
     """Create JWT refresh token."""
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.REFRESH_TOKEN_EXPIRE_MINUTES)
     
     to_encode.update({"exp": expire, "type": "refresh"})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
